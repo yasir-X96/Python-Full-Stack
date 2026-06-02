3a7a665143c7d2c6cbfa9019260fcc2f072aa595
yasir-X96 <my666243@gmail.com>
2026-06-02 13:49:45 +0530

    Add files via upload

---
+++ smartHR365/utils/fileHandler.py
@@ -0,0 +1,19 @@
+import json
+import os
+
+
+FILE_PATH="data/employees.json"
+
+def read_employees():
+    if not os.path.exists(FILE_PATH):
+        return []
+    with open(FILE_PATH,"r")as file:
+        try:
+            return json.load(file)
+        except json.JSONDecodeError:
+            return []
+
+
+def save_employees(employee_list):
+    with open(FILE_PATH,"w") as file:
+        json.dump(employee_list,file,indent=4)
